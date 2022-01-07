9a8ee0fd7b96f29d91432d8cf33d19a0be76f5da
Malloc Studio <46115200+MallocStudio@users.noreply.github.com>
2022-01-07 16:05:57 +1100

    fixed a rendering issue with dynamic glyphs

--- src/types.c
+++ src/types.c
@@ -122,8 +122,21 @@ void uninit_glyphs (Glyphs *glyphs) {
 void glyphs_generate_text (Text *result, SDL_Renderer *renderer, Glyphs *glyphs, const char *string) {
     int width;
     int height;
+    Uint32 rmask, gmask, bmask, amask;
+    // SDL interprets each pixel as a 32-bit number, so our masks must depend on the endianness (byte order) of the machine
+    #if SDL_BYTEORDER == SDL_BIG_ENDIAN
+        rmask = 0xff000000;
+        gmask = 0x00ff0000;
+        bmask = 0x0000ff00;
+        amask = 0x000000ff;
+    #else
+        rmask = 0x000000ff;
+        gmask = 0x0000ff00;
+        bmask = 0x00ff0000;
+        amask = 0xff000000;
+    #endif
     TTF_SizeText(glyphs->font, string, &width, &height);
-    SDL_Surface *result_surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0); // @incomplete come back to this function and understand it better
+    SDL_Surface *result_surface = SDL_CreateRGBSurface(0, width, height, 32, rmask, gmask, bmask, amask);
     int offset_from_prev_glyph = 0;
     for (int i = 0; i < strlen(string); ++i) {
         int glyph_index = UNICODE_TO_GLYPH_INDEX((int)string[i]);
